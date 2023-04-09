ac3923b4a491e174d32a76082095c773c5ab7a2c
Ross Gill <ross.gill.10@gmail.com>
2023-04-08 22:32:40 -0500

    Initial Commit

---
+++ rsgcognitoauth/rsgcognitoauthApp.swift
@@ -0,0 +1,17 @@
+//
+//  rsgcognitoauthApp.swift
+//  rsgcognitoauth
+//
+//  Created by Ross Gill on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct rsgcognitoauthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
